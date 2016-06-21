e64583a7802390c6033ff550a0e34936b076a7be
Fabian Greif <Fabian.Greif@dlr.de>
2016-06-21 15:33:24 +0000

    [pdoc] Improve indention detection algorithm for description tags.
    Now also considers the second line if the first was not indented. This helps with the following constructs:
    
      <description>This is some text
         which is only indented after
         the second line.</description>

--- pdoc/parser/common.py
+++ pdoc/parser/common.py
@@ -12,6 +12,13 @@ def parse_text(node, tag, default_value=None):
     """
     Removes indentation from text tags.
     
+    If the first line does not contain an indentation the next line is also
+    checked. This helps e.g. for the following case:
+    
+        <description>This is some
+          text which is indentated only after
+          the second line.</description>
+    
     Keyword arguments:
     node -- Start node for the search
     tag -- XML tag name to look for in the XML node
@@ -19,17 +26,24 @@ def parse_text(node, tag, default_value=None):
     """
     text = node.findtext(tag, default_value)
 
+    second_line_test = False
     if text is not None:
         to_strip = None
         lines = []
         for line in text.split('\n'):
-            if to_strip is None:
+            if to_strip is None or second_line_test:
                 if line == '' or line.isspace():
                     continue
 
                 # First line which is not only whitespace
-                match = re.match(r'^(\s*)', text)
+                match = re.match(r'^(\s*)', line)
                 to_strip = match.group(1)
+                
+                if second_line_test:
+                    second_line_test = False
+                else:
+                    if to_strip == "" or to_strip is None:
+                        second_line_test = True
 
             lines.append(line.lstrip(to_strip))
         text = ("\n".join(lines)).rstrip()
