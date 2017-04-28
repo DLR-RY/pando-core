523d0c76639a9bdf06e0ea06097e06eb7498e643
Fabian Greif <Fabian.Greif@dlr.de>
2017-04-28 20:11:17 +0200

    Use scripts as pando package.

---
+++ pando/__main__.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017, German Aerospace Center (DLR)
+#
+# This file is part of the development version of the pando library.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Authors:
+# - 2017, Fabian Greif (DLR RY-AVS)
+
+import argparse
+
+import pando.scripts
+from subprocess import SubprocessError
+
+def main():
+    arg = argparse.ArgumentParser(prog='pando',
+                                  description='pando tool suite')
+
+    def print_usage(argv):
+        """ Print a help message if no command has been selected. """
+        arg.print_help()
+
+    arg.set_defaults(function=print_usage)
+
+    subparsers = arg.add_subparsers()
+
+    parser_assistant = subparsers.add_parser('assistant')
+    parser_assistant.set_defaults(function=pando.scripts.assistant.main)
+
+    parser_calibration = subparsers.add_parser('calibration_csv')
+    parser_calibration.set_defaults(function=pando.scripts.calibration_csv.main)
+
+    parser_indent = subparsers.add_parser('indent')
+    parser_indent.set_defaults(function=pando.scripts.indent.main)
+
+    parser_latex = subparsers.add_parser('latex')
+    parser_latex.set_defaults(function=pando.scripts.latex.main)
+
+    parser_structure = subparsers.add_parser('structure')
+    parser_structure.set_defaults(function=pando.scripts.structure.main)
+
+    parser_svg = subparsers.add_parser('svg')
+    parser_svg.set_defaults(function=pando.scripts.svg.main)
+
+    parser_verify = subparsers.add_parser('verify')
+    parser_verify.set_defaults(function=pando.scripts.verify.main)
+
+    args, remaining_args = arg.parse_known_args()
+    args.function(remaining_args)
---
+++ pando/scripts/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017, German Aerospace Center (DLR)
+#
+# This file is part of the development version of the pando library.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Authors:
+# - 2017, Fabian Greif (DLR RY-AVS)
+
+from . import assistant
+from . import calibration_csv
+from . import indent
+from . import latex
+from . import structure
+from . import svg
+from . import verify
